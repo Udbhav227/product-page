703e9bc692ceebdc0d0048dae4f1c6646cbe0e35
Udbhav <udbhav227@gmail.com>
2025-10-16 13:42:37 +0000

    refractor Pagination logic to display less page links

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -1,5 +1,5 @@
-import styled from 'styled-components';
-import { Link, useSearchParams } from 'react-router-dom';
+import styled from "styled-components";
+import { Link, useSearchParams } from "react-router-dom";
 
 const PaginationContainer = styled.div`
   display: flex;
@@ -15,8 +15,8 @@ const PageLink = styled(Link)`
   color: #333;
   text-decoration: none;
   border-radius: 4px;
-  background-color: ${(props) => (props.$active ? '#f0f0f0' : 'white')};
-  font-weight: ${(props) => (props.$active ? 'bold' : 'normal')};
+  background-color: ${(props) => (props.$active ? "#f0f0f0" : "white")};
+  font-weight: ${(props) => (props.$active ? "bold" : "normal")};
 
   &:hover {
     background-color: #f0f0f0;
@@ -29,64 +29,51 @@ const Ellipsis = styled.span`
 
 const Pagination = ({ totalPages }) => {
   const [searchParams] = useSearchParams();
-  const currentPage = parseInt(searchParams.get('page') || '1', 10);
+  const currentPage = parseInt(searchParams.get("page") || "1", 10);
 
   const renderPageNumbers = () => {
     const pageNumbers = [];
-    const maxPagesToShow = 5;
 
-    if (totalPages <= maxPagesToShow) {
-      for (let i = 1; i <= totalPages; i++) {
-        pageNumbers.push(
-          <PageLink key={i} to={`/?page=${i}`} $active={i === currentPage}>
-            {i}
-          </PageLink>,
-        );
-      }
-    } else {
-      if (currentPage > 1) {
-        pageNumbers.push(
-            <PageLink key="prev" to={`/?page=${currentPage - 1}`}>
-                &lt;
-            </PageLink>
-        )
-      }
+    if (currentPage > 1) {
+      pageNumbers.push(
+        <PageLink key="prev" to={`/?page=${currentPage - 1}`}>
+          &lt;
+        </PageLink>
+      );
+    }
+
+    if (totalPages > 1 && currentPage !== 1) {
       pageNumbers.push(
         <PageLink key={1} to="/?page=1" $active={1 === currentPage}>
           1
-        </PageLink>,
+        </PageLink>
       );
+    }
 
-      if (currentPage > 3) {
+    if (currentPage > 1 && currentPage < totalPages) {
+      // Add ellipsis if the current page is far from the first page (optional, but nice)
+      if (currentPage > 2) {
         pageNumbers.push(<Ellipsis key="start-ellipsis">...</Ellipsis>);
       }
 
-      let startPage = Math.max(2, currentPage - 1);
-      let endPage = Math.min(totalPages - 1, currentPage + 1);
+      pageNumbers.push(
+        <PageLink key={currentPage} to={`/?page=${currentPage}`} $active={true}>
+          {currentPage}
+        </PageLink>
+      );
 
-      if (currentPage <= 2) {
-        startPage = 2
-        endPage = 4
-      }
-
-      if (currentPage >= totalPages -1) {
-          startPage = totalPages -3
-          endPage = totalPages -1
-      }
-
-
-      for (let i = startPage; i <= endPage; i++) {
-        pageNumbers.push(
-          <PageLink key={i} to={`/?page=${i}`} $active={i === currentPage}>
-            {i}
-          </PageLink>,
-        );
-      }
-
-      if (currentPage < totalPages - 2) {
+      if (currentPage < totalPages - 1) {
         pageNumbers.push(<Ellipsis key="end-ellipsis">...</Ellipsis>);
       }
+    } else {
+      pageNumbers.push(
+        <PageLink key={currentPage} to={`/?page=${currentPage}`} $active={true}>
+          {currentPage}
+        </PageLink>
+      );
+    }
 
+    if (totalPages > 1 && currentPage !== totalPages) {
       pageNumbers.push(
         <PageLink
           key={totalPages}
@@ -94,16 +81,16 @@ const Pagination = ({ totalPages }) => {
           $active={totalPages === currentPage}
         >
           {totalPages}
-        </PageLink>,
+        </PageLink>
       );
+    }
 
-      if (currentPage < totalPages) {
-        pageNumbers.push(
-            <PageLink key="next" to={`/?page=${currentPage + 1}`}>
-                &gt;
-            </PageLink>
-        )
-      }
+    if (currentPage < totalPages) {
+      pageNumbers.push(
+        <PageLink key="next" to={`/?page=${currentPage + 1}`}>
+          &gt;
+        </PageLink>
+      );
     }
 
     return pageNumbers;
